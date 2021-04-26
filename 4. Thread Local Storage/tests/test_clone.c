a1ac4fe469a02331f458bfa9accd19504a9ccc52
Ivan Isakov <isakovi@bu.edu>
2021-04-25 21:29:26 -0400

    Update

--- 4. Thread Local Storage/tests/test_clone.c
+++ 4. Thread Local Storage/tests/test_clone.c
@@ -8,22 +8,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define LENGTH 14
-#define THREAD_CNT 5
+#define LENGTH 15
+#define THREAD_CNT 10
 #define COUNTER_FACTOR 0xFFFFFF
 
 #define BUFFER_SIZE 10000000
 
-// const char *msg = "Hi, my name is In this project, we are implementing a library that  Hi, my name is In this project, we are implementing a library that provides protected memory regions for threads, which can be used for local storage. Recall that all threads share the same memory address space. This can be helpful for programs that need to share information across threads and don’t want to repeatedly copy every modification back and forth, but it can also be problematic when one thread modifies information that another thread assumed would remain unmodified. Hi, my name is In this project, we are implementing a library that provides protected memory regions for threads, which can be used for local storage. Recall that all threads share the same memory address space. This can be helpful for programs that need to share information across threads and don’t want to repeatedly copy every modification back and forth, but it can also be problematic when one thread modifies information that another thread assumed would remain unmodified";
-const char *msg = "Hi my name is";
-const char *msg2 = "Ivan";
+const char *msg = "Hi, my name is";
 
 int ret;
 int count = 0;
 pthread_t tid;
 pthread_barrier_t barrier;
 char buffer[BUFFER_SIZE] = {0};
-
 
 void wasteTime(int a){
 	for(long int i = 0; i < a*COUNTER_FACTOR; i++);
@@ -40,29 +37,19 @@ void* clone(void *arg){
     pthread_barrier_wait(&barrier);
 
     // read from the clone tls
-    printf("Thread %lx: Trying to read the message from clone\n", pthread_self());
-    ret = tls_read(0, BUFFER_SIZE, buffer);
+    printf("Thread %lx: Trying to read the message\n", pthread_self());
+    ret = tls_read(0, LENGTH, buffer);
     assert(ret == 0);
 
-    printf("Read buffer: %s\n", buffer);
     // Use memcmp to check the validity -> strings.h
-    // assert(memcmp(msg, buffer, LENGTH) == 0);
+    int n = memcmp(msg, buffer, LENGTH);
+    assert(n == 0);
 
-    printf("Thread %lx: Read successful. Read message: %s\n", pthread_self(), buffer);
-
-    printf("Writing to tls: %s\n", msg2);
-    assert(tls_write(0, 5, msg2) == 0);
-    
-    printf("Thread %lx: Trying to read the message from write\n", pthread_self());
-    assert(tls_read(0, 5, buffer) == 0);
-    
-    assert(memcmp(msg2, buffer, 5) == 0);
     printf("Thread %lx: Read successful. Read message: %s\n", pthread_self(), buffer);
 
 	printf("Thread %lx finished\n\n",pthread_self());
     count++;
-    assert(tls_destroy() == 0);
-
+    tls_destroy();
 	return NULL;
 }
 
@@ -78,12 +65,9 @@ int main(int argc, char **argv){
     tid = pthread_self();
 	
     // write some data
-    memset(buffer, 'a', BUFFER_SIZE);
-    // printf("Buffer: %s\n", buffer);
-
     printf("Main: Trying to write string %s to TLS...\n", msg);
     ret = tls_write(0, LENGTH, msg);
-    // printf("Buffer after main: %s\n", buffer);
+    
     assert(ret == 0);
     printf("...Main: Write successful\n\n");
 
@@ -92,23 +76,6 @@ int main(int argc, char **argv){
     for (int i = 0; i < THREAD_CNT; i++) {
 		pthread_create(&threads[i], NULL, &clone, (void *)(intptr_t)i);
 	}
-
-
-    for(long int i = 0; i < THREAD_CNT; i++){
-		wasteTime(10);
-	}
-
-    assert(tls_read(0, LENGTH, buffer) == 0);
-    
-    
-    printf("Thread main: Read successful. Read message: %s\n", buffer);
-
-    assert(tls_write(0, 5, msg2) == 0);
-    
-    printf("Thread main: Trying to read the message from write\n");
-    assert(tls_read(0, 5, buffer) == 0);
-    assert(memcmp(msg2, buffer, 5) == 0);
-    printf("Thread main: Read successful. Read message: %s\n", buffer);
 
     for(long int i = 0; i < THREAD_CNT; i++){
 		wasteTime(10);
